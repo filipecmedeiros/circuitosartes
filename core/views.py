9b180d31e14026ffd63a70fe866b3cf0a2f323b3
Filipe Medeiros <fcma@cin.ufpe.br>
2017-07-21 01:54:29 -0300

    Adicionando formulário de artista

--- core/views.py
+++ core/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render
 
+from .models import Artist, Style, State, City
+
+from .forms import ArtistForm
+
 # Create your views here.
 def index (request):
 
-	return render (request, 'index.html')
+	if request.method == 'POST':
+		artist_form = ArtistForm(request.POST)
+
+		if artist_form.is_valid():
+			name = artist_form.cleaned_data['name']
+			gener = Style.objects.get(id = artist_form.cleaned_data['gener'])
+			state = State.objects.get(id = artist_form.cleaned_data['state'])
+			city = City.objects.get(id = artist_form.cleaned_data['city'])
+			neighborhood = artist_form.cleaned_data['neighborhood']
+			address = artist_form.cleaned_data['address']
+			price = artist_form.cleaned_data['price']
+			contact = artist_form.cleaned_data['contact']
+
+			Artist.objects.create(name = name, 
+				gener=gener, 
+				state = state,
+				city = city,
+				neighborhood = neighborhood,
+				address = address,
+				price=price,
+				contact = contact,
+				)
+
+	else:
+		artist_form = ArtistForm()
+	
+	context = {
+		'artist_form' : artist_form,
+	}
+
+	return render (request, 'index.html', context)
